60612c37e193cd3164c18313490600937b6e0b7e
Denys Pronin <denispronindev@gmail.com>
2024-06-24 13:29:04 +0300

    Upd app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,19 +23,17 @@ function App() {
   const totalFeedback = values.good + values.neutral + values.bad;
 
   const updateFeedback = (feedbackType) => {
-    if (feedbackType === "good") {
-      setValue({ ...values, good: values.good + 1 });
-    } else if (feedbackType === "bad") {
-      setValue({ ...values, bad: values.bad + 1 });
-    } else if (feedbackType === "neutral") {
-      setValue({ ...values, neutral: values.neutral + 1 });
-    } else if (feedbackType === "reset") {
-      setValue({ good: 0, neutral: 0, bad: 0 });
-    }
+    setValue(
+      feedbackType === "reset"
+        ? { good: 0, neutral: 0, bad: 0 }
+        : { ...values, [feedbackType]: values[feedbackType] + 1 }
+    );
   };
+
   useEffect(() => {
     window.localStorage.setItem("Reviews", JSON.stringify(values));
   }, [values]);
+
   return (
     <>
       <Description />
